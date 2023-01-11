52548014f3b0babab0fc0dadae69deb16682d0f1
dossantosloic <dossantosloic94@gmail.com>
2023-01-11 23:22:10 +0100

    Modified helloworld, Added CMakeLists

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int argc, char const* argv[]) {
-    std::cout << "Hello world!" << std::endl;
+    // int var;
+    // std::cout << "Hello world!" << std::endl;
+    std::string var;
+
+    std::cout << "What's your name?" << std::endl;
+    std::cin >> var;
+    std::cout << "Hello " << var << "!" << std::endl;
     return 0;
 }
