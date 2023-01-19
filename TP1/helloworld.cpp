4e030a3361544bc41af3879b014e6873809bec71
dossantosloic <dossantosloic94@gmail.com>
2023-01-19 15:13:05 +0100

    Saved changes

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -8,5 +8,6 @@ int main(int argc, char const* argv[]) {
     std::cout << "What's your name?" << std::endl;
     std::cin >> var;
     std::cout << "Hello " << var << "!" << std::endl;
+
     return 0;
 }
