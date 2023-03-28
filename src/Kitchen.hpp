c02e94326722e159c1fad6fc0ccfafa52f87cae9
DOS SANTOS LOIC <loic.dos-santos@heckel02.u-pem.fr>
2023-03-28 13:26:26 +0200

    TP8

--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,30 @@
 #pragma once
 
+#include <string>
+#include <memory>
+#include <iostream>
+#include <vector>
+
+#include "../lib/Unit.hpp"
+
 class Kitchen
-{};
+{
+private:
+    std::vector<Unit*> _units{};
+public:
+    Kitchen() {};
+
+    const Unit& register_unit(Unit unit) {
+        _units.emplace(_units.end(), &unit);
+        return unit;
+    }
+
+    const Unit* find_unit(const std::string &s) {
+        for(auto it = _units.begin(); it != _units.end(); it++) {
+            if((*it)->name == s) {
+                return *it;
+            }
+        }
+        return nullptr;
+    }
+};
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include "Kitchen.hpp"
+
 int main(int argc, char** argv)
 {
     // You can implement whatever you want here!
+    if(argv[argc - 1]) {
+    }
+    auto kitchen = Kitchen {};
+
+    const auto& piece_unit = kitchen.register_unit(Unit { "" });
+    std::cout << piece_unit.name << std::endl;
+
+    const auto& ml_unit = kitchen.register_unit(Unit { "ml" });
+    std::cout << ml_unit.name << std::endl;
+
+    const auto& g_unit = kitchen.register_unit(Unit { "g" });
+    std::cout << g_unit.name << std::endl;
     return 0;
 }
